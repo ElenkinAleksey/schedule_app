c60e41a892b4646cee725e3ebf002f6cb32c5ff3
Aleksey Elenkin <elenkinaleksey@gmail.com>
2016-09-12 22:54:28 +0300

    Added scaffold Auditory

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :auditories
   resources :pairs
   resources :subjects
   # The priority is based upon order of creation: first created -> highest priority.
